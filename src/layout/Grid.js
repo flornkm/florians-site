d53216a19f3a1dae96faecbb0ada526f67374754
flooorianflo <florian.kiem@hfg.design>
2023-02-14 12:31:18 +0100

    Save

--- src/layout/Grid.js
+++ src/layout/Grid.js
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 const Grid = ({ children, columns }) => {
     return (
-        <div class={"grid grid-cols-3 grid-rows-2 gap-4 max-w-80rem min-h-[50vh]"}>
+        <div class={"grid grid-cols-3 grid-rows-2 gap-4 max-w-80rem min-h-[50vh] max-md:min-h-screen max-md:grid-cols-1"}>
             {children}
         </div>
     );
