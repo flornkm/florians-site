7355dab6d5d9e0447f08ccdf250961fafc9b37f9
flooorianflo <florian.kiem@hfg.design>
2023-05-14 20:44:59 +0200

    404 Animation

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,23 @@
 import Link from "next/link";
 import Footer from "@/components/Footer";
+import RiveComponent from "@rive-app/react-canvas";
 
 export default function Custom404() {
     return (
         <>
-            <div className="flex flex-col items-center gap-4 justify-center h-[70vh]">
-                <h1 className="text-[96px] font-mono font-bold text-transparent bg-gradient-to-t from-sky-700 to-black bg-clip-text dark:from-blue-300 dark:to-white">404</h1>
+            <div className="flex flex-col items-center gap-2 justify-center h-screen">
+            <div className="w-full md:pt-20 pb-4 max-md:h-[400px] relative z-0">
+                <RiveComponent
+                    artboard="404"
+                    src="./animations/404.riv"
+                    className="w-full h-[400px] object-contain m-auto"
+                />
+            </div>
                 <Link
                     className="bg-[#1280EC] text-white font-medium sm:pr-4 sm:pl-4 sm:pt-2 sm:pb-2 max-sm:p-2 rounded-md hover:bg-[#2795FE] transition-all flex"
                     href="/"
                 >
-                    Back to Home
+                    Return Home
                 </Link>
             </div>
             <Footer />
