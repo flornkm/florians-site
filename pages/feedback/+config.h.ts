e5071fa2c626be0d019f1a12cf74264912735429
flooorianflo <florian.kiem@hfg.design>
2023-11-16 22:30:09 +0100

    feat: add opengraph image for feedback

--- pages/feedback/+config.h.ts
+++ pages/feedback/+config.h.ts
@@ -1,4 +1,7 @@
 export default {
   title: "Feedback | Florian - Design Engineer",
   fullpage: true,
+  description: "A page for receiving feedback.",
+  image: "/images/opengraph/og-image-feedback.jpg",
+  noindex: true,
 }
