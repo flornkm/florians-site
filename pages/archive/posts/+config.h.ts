4fe1e4abac8c92e68c2283b1db35a79a59ee9077
flooorianflo <florian.kiem@hfg.design>
2024-01-22 01:27:38 +0100

    chore: fix posts seo

--- pages/archive/posts/+config.h.ts
+++ pages/archive/posts/+config.h.ts
@@ -1,5 +1,5 @@
 export default {
-  title: "Photos Archive | Florian - Design Engineer",
-  description: "My latest photos from my archive.",
+  title: "Posts Archive | Florian - Design Engineer",
+  description: "The latest posts of my archive.",
   image: "/images/opengraph/og-image-archive.jpg",
 }
