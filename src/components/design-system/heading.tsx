ab0119c44c01ee5c37bd8e795f68bd1ded607956
Envel <no-reply@envel.dev>
2026-03-08 13:10:58 +0000

    Increase H2 heading size from text-sm to text-base

--- src/components/design-system/heading.tsx
+++ src/components/design-system/heading.tsx
@@ -10,7 +10,7 @@ export const headingVariants = cva("font-semibold text-primary", {
   variants: {
     size: {
       h1: "text-base font-semibold",
-      h2: "text-sm font-semibold",
+      h2: "text-base font-semibold",
       h3: "text-sm font-semibold",
       h4: "text-sm font-medium",
       h5: "text-xs font-medium",
