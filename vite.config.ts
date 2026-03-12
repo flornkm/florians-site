683a413ccf71a0bb0162734cd2ea6ffe1badf5d4
flornkm <hello@floriankiem.com>
2026-03-12 23:53:58 +0400

    fix: use nitro

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -17,6 +18,7 @@ export default defineConfig({
       }),
     },
     tanstackStart(),
+    nitro(),
     react(),
     tailwindcss(),
   ],
